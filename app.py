3eee8c57962ebc013c07348689f0424bb095c4e4
Robert Kim <rkty13@gmail.com>
2016-12-29 23:56:20 -0500

    Completed content.

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app.config.from_pyfile("config.py")
 
 @app.route("/")
 def index():
-    return render_template("index.html", host = app.config["STATIC_HOST"])
+    return render_template("index.html", 
+            host = app.config["STATIC_HOST"],
+            scrobble_key = app.config["SCROBBLE_KEY"])
 
 if __name__ == "__main__":
     app.run(debug = app.config["DEBUG"])
