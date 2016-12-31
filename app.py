84671471b6ac2b0bc7e399db9aef736791457854
Robert Kim <rkty13@gmail.com>
2016-12-31 15:07:23 -0500

    Added resume.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def index():
     return render_template("index.html", 
             host = app.config["STATIC_HOST"],
             scrobble_key = app.config["SCROBBLE_KEY"])
+
+@app.route("/resume.pdf")
+@app.route("/resume")
+def resume():
+    return redirect("https://s3.amazonaws.com/rkty13-personalsite/resume.pdf")
 
 if __name__ == "__main__":
     app.run(debug = app.config["DEBUG"])
